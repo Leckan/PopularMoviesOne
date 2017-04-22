3601791e2c507de0aa967467a3bcce479ef9929f
ahmed <amustapha@appzonegroup.com>
2017-04-22 23:58:23 +0100

    Updated To RecyclerView

--- app/src/main/java/com/leckan/popularmoviesone/UI/MainActivity.java
+++ app/src/main/java/com/leckan/popularmoviesone/UI/MainActivity.java
@@ -1,5 +1,12 @@
 package com.leckan.popularmoviesone.UI;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -11,25 +18,32 @@ import com.leckan.popularmoviesone.Adapter.MovieAdapter;
 import com.leckan.popularmoviesone.Model.DummyMovies;
 import com.leckan.popularmoviesone.Model.Movie;
 import com.leckan.popularmoviesone.R;
+import com.leckan.popularmoviesone.Utilities.NetworkUtils;
 import com.leckan.popularmoviesone.Utilities.Utility;
 
+import java.net.URL;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements MovieAdapter.ListItemClickListener{
+public class MainActivity extends AppCompatActivity implements MovieAdapter.ListItemClickListener {
 
+    ConnectivityManager mConMgr;
     RecyclerView moviesList;
     MovieAdapter adapter;
+    private String sPreferredType;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         moviesList = (RecyclerView) findViewById(R.id.recycler_view);
 
         int mNoOfColumns = Utility.calculateNoOfColumns(getApplicationContext());
-
+        mConMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         List<Movie> Movies = DummyMovies.getMovieList();
-        GridLayoutManager layoutManager =  new GridLayoutManager(this,mNoOfColumns);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, mNoOfColumns);
 
         moviesList.setLayoutManager(layoutManager);
         adapter = new MovieAdapter(Movies, this);
@@ -38,10 +52,40 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.List
     }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        sPreferredType = preferences.getString("chosenSortType", "Popularity");
+        NetworkInfo networkInfo = mConMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoadMoviesPage(sPreferredType);
+        } else {
+            LoadDefaultPage();
+        }
+    }
+
+    private void LoadDefaultPage() {
+    }
+
+    public void LoadMoviesPage(String prefType) {
+        new DownloadMoviesTask().execute();
+    }
+
+    private class DownloadMoviesTask extends AsyncTask<Object, Object, Void> {
 
 
+        @Override
+        protected Void doInBackground(Object... voids) {
 
+            URL movieURL = NetworkUtils.buildUrl(sPreferredType);
+            String jsonStr = NetworkUtils.makeServiceCall(movieURL.toString());
 
+            return null;
+        }
+    }
 
 
     @Override
@@ -50,11 +94,13 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.List
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemThatWasClickedId = item.getItemId();
         if (itemThatWasClickedId == R.id.action_settings) {
-            //makeGithubSearchQuery();
+            Intent settingsIntent = new Intent(getBaseContext(), SettingsActivity.class);
+            startActivity(settingsIntent);
             return true;
         }
         return super.onOptionsItemSelected(item);
